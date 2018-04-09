c413b39e57ff3c3fdb26fa306800da08db1f8e6e
dequinox <umarov.doro@gmail.com>
2018-04-09 19:45:03 +0300

    First commit: base64, helper, keyconstants, main, timer

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+// #include <windows.h> // for windows
+//#include "IO.h"
+#include "Timer.h"
+#include "Base64.h"
+#include "Helper.h"
+#include "KeyConstants.h"
+
+using namespace std;
+
+int main()
+{
+      /** For windows OS **/
+      /*
+      MSG Msg;
+
+      while (GetMessage (&Msg, NULL, 0, 0)) // Retrieves a message from the calling thread's message queue.
+      {
+            TranslateMessage(&Msg); // Translates virtual-key messages into character messages.
+            DispatchMessage(&Msg); // Dispatches a message to a window procedure.
+      }
+      */
+      return 0;
+}
